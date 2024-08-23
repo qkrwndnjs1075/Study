242b1658721dda6ecd520796ea3b467a6403a0f5
박주원 <qkrwndnjs7308@gmail.com>
2024-08-23 17:30:10 +0900

    e

--- Spring/Spring/src/main/java/com/example/spring/global/error/GlobalExceptionHandler.java
+++ Spring/Spring/src/main/java/com/example/spring/global/error/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.example.spring.global.error;
 
+import com.example.spring.global.error.exception.ErrorCode;
 import com.example.spring.global.error.exception.PageException;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,5 +12,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(PageException.class)
-    public ResponseEntity<>
+    public ResponseEntity<ErrorResponse> handlePageException(PageException e){
+        ErrorCode errorCode = e.getErrorCode();
+        ErrorResponse response = ErrorResponse.of(errorCode, errorCode.getMessage());
+        e.printStackTrace();
+
+        return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatusCode()));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
 }
