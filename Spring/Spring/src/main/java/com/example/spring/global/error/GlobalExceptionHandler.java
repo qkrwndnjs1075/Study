722ff97860c9c758ee793048ce3837dd0c2d903b
박주원 <qkrwndnjs7308@gmail.com>
2024-08-23 21:56:19 +0900

    e

--- Spring/Spring/src/main/java/com/example/spring/global/error/GlobalExceptionHandler.java
+++ Spring/Spring/src/main/java/com/example/spring/global/error/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.example.spring.global.error.exception.ErrorCode;
 import com.example.spring.global.error.exception.PageException;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -21,4 +22,22 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException e){
+
+        ErrorCode errorCode = ErrorCode.BAD_REQUEST;
+        ErrorResponse response = ErrorResponse.of(errorCode, errorCode.getMessage());
+        e.printStackTrace();
+
+        return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatusCode()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e){
+
+        ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+        ErrorResponse response = ErrorResponse.of(errorCode, errorCode.getMessage());
+        e.printStackTrace();
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
