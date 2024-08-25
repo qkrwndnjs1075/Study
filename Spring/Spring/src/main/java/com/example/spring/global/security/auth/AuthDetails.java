3c39d5b216743a2d2276d2690d2c61e81cf4f134
박주원 <qkrwndnjs7308@gmail.com>
2024-08-25 18:47:30 +0900

    AuthDetail

--- Spring/Spring/src/main/java/com/example/spring/global/security/auth/AuthDetails.java
+++ Spring/Spring/src/main/java/com/example/spring/global/security/auth/AuthDetails.java
@@ -2,12 +2,51 @@ package com.example.spring.global.security.auth;
 
 import com.example.spring.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 
 public record AuthDetails(User user) implements UserDetails {
 
     @Override
+    public Collection<? extends GrantedAuthority> getAuthories() {
+        return new ArrayList<>(Collections.singleton(new SimpleGrantedAuthority("ROLE"+user.getRole().toString())));
+    }
+
+    @Override
+    public String getPassword(){
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername(){
+        return user.getAccountId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 
 }
