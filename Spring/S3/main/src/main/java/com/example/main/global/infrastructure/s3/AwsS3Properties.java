db9a4a6d5e29b2b66c4b83e669f25465ad161341
박주원 <qkrwndnjs7308@gmail.com>
2024-10-09 19:57:46 +0900

    공부

---
+++ Spring/S3/main/src/main/java/com/example/main/global/infrastructure/s3/AwsS3Properties.java
@@ -0,0 +1,14 @@
+package com.example.main.global.infrastructure.s3;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "aws.s3")
+public record AwsS3Properties (
+        String bucket,
+        String url,
+        String profileFolder,
+        String blogFolder
+)
+{
+
+}
