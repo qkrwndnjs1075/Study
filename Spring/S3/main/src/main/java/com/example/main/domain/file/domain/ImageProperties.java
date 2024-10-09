74e838461f5d24b2af2db86164dd0a3db6cb63e0
박주원 <qkrwndnjs7308@gmail.com>
2024-10-09 19:57:31 +0900

    공부

---
+++ Spring/S3/main/src/main/java/com/example/main/domain/file/domain/ImageProperties.java
@@ -0,0 +1,7 @@
+package com.example.main.domain.file.domain;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "default.profile")
+public record ImageProperties(String ImageUrl) {
+}
