a3e86048dc401b8c34962f10711b97220ea8c549
박주원 <qkrwndnjs7308@gmail.com>
2024-10-11 20:16:27 +0900

    swagger 그룹화

---
+++ Spring/S3/main/src/main/java/com/example/main/global/common/swagger/SwaggerConfig.java
@@ -0,0 +1,63 @@
+package com.example.main.global.common.swagger;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.core.jackson.ModelResolver;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+        @Bean
+        public OpenAPI openAPI() {
+            return new OpenAPI()
+                    .addSecurityItem(new SecurityRequirement().addList("JWT"))
+                    .components(new Components().addSecuritySchemes("JWT", createAPIKeyScheme()))
+                    .info(apiInfo());
+        }
+
+        @Bean
+        public ModelResolver modelResolver(ObjectMapper objectMapper) {
+            return new ModelResolver(objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE));
+        }
+
+        private SecurityScheme createAPIKeyScheme() {
+            return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                    .bearerFormat("JWT")
+                    .scheme("bearer");
+        }
+
+        private Info apiInfo() {
+            return new Info()
+                    .title("Spring Boot REST API Specifications")
+                    .description("Specification")
+                    .version("1.0.0");
+        }
+
+    @Bean
+    public GroupedOpenApi getAuthApi() {
+        return createGroupedOpenApi("blog", "/blog/**");
+    }
+
+    @Bean
+    public GroupedOpenApi getFeedbackApi() {
+        return createGroupedOpenApi("intern", "/intern/**");
+    }
+
+
+    private GroupedOpenApi createGroupedOpenApi(String groupName, String path) {
+        return GroupedOpenApi.builder()
+                .group(groupName)
+                .pathsToMatch(path)
+                .build();
+    }
+
+
+
+}
