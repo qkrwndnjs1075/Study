acbbecaa4a335f622f1c86e6026f1f9818594672
박주원 <qkrwndnjs7308@gmail.com>
2024-10-13 21:07:08 +0900

    공부

--- Spring/feign-client/src/main/java/com/example/feignclient/infrustrate/feign/client/Test.java
+++ Spring/feign-client/src/main/java/com/example/feignclient/infrustrate/feign/client/Test.java
@@ -1,2 +1,12 @@
-package com.example.feignclient.infrustrate.feign.client;public class Test {
+package com.example.feignclient.infrustrate.feign.client;
+
+import com.example.feignclient.infrustrate.feign.infrustructure.RetryConfiguration;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name="아몰랑", url="url", configuration = RetryConfiguration.class)
+public interface Test {
+
+    @GetMapping("/test")
+    // 작성
 }
