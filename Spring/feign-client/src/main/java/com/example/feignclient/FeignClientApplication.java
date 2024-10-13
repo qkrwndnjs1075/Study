32a561a85136d7931946af3881d36163ce818ad6
박주원 <qkrwndnjs7308@gmail.com>
2024-10-13 21:07:12 +0900

    공부

--- Spring/feign-client/src/main/java/com/example/feignclient/FeignClientApplication.java
+++ Spring/feign-client/src/main/java/com/example/feignclient/FeignClientApplication.java
@@ -1,11 +1,16 @@
 package com.example.feignclient;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @EnableFeignClients
+@ConfigurationPropertiesScan
 @SpringBootApplication
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class FeignClientApplication {
 
     public static void main(String[] args) {
