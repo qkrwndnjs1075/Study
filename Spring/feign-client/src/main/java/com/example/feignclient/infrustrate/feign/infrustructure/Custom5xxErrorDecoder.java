9623fbee3db658c5a17feca4a92fda6793aeb110
박주원 <qkrwndnjs7308@gmail.com>
2024-10-13 20:39:26 +0900

    feign 공부

---
+++ Spring/feign-client/src/main/java/com/example/feignclient/infrustrate/feign/infrustructure/Custom5xxErrorDecoder.java
@@ -0,0 +1,28 @@
+package com.example.feignclient.infrustrate.feign.infrustructure;
+
+import feign.Response;
+import feign.RetryableException;
+import feign.codec.Decoder;
+import feign.codec.ErrorDecoder;
+
+public class Custom5xxErrorDecoder implements ErrorDecoder {
+
+    private final ErrorDecoder defaultErrorDecoder = new Default();
+
+    @Override
+    public Exception decode(String methodKey, Response response){
+        var exception = defaultErrorDecoder.decode(methodKey,response);
+
+        if(response.status() >= 500){
+            return new RetryableException(
+                    response.status(),
+                    exception.getMessage(),
+                    response.request().httpMethod(),
+                    exception,
+                    (Long) null,
+                    response.request()
+            );
+        }
+        return exception;
+    }
+}
