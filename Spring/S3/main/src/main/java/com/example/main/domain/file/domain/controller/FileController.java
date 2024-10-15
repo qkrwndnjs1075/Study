73514b2ec660224620b4e99f68968929ad01eac1
박주원 <qkrwndnjs7308@gmail.com>
2024-10-15 17:09:18 +0900

    공부공부

---
+++ Spring/S3/main/src/main/java/com/example/main/domain/file/domain/controller/FileController.java
@@ -0,0 +1,24 @@
+package com.example.main.domain.file.domain.controller;
+
+import com.example.main.domain.file.domain.dto.FileResponse;
+import com.example.main.domain.file.domain.type.ImageType;
+import com.example.main.domain.file.service.ImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/file")
+public class FileController {
+
+    private final ImageService imageService;
+
+    @PostMapping("/image")
+    FileResponse uploadImage(@RequestPart("file") MultipartFile multipartFile, @RequestParam("type") ImageType imagetype){
+        String path = imageService.saveImage(multipartFile, imagetype);
+        return new FileResponse(imageService.getFileBaseUrl() +path, multipartFile.getOriginalFilename());
+    }
+
+}
