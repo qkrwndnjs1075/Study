8a3caec31eed287fa2a628b4c73452fb81c09c66
박주원 <qkrwndnjs7308@gmail.com>
2024-10-15 17:09:12 +0900

    공부공부

--- Spring/S3/main/src/main/java/com/example/main/MainApplication.java
+++ Spring/S3/main/src/main/java/com/example/main/MainApplication.java
@@ -1,11 +1,15 @@
 package com.example.main;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableFeignClients
 public class MainApplication {
 
     public static void main(String[] args) {
