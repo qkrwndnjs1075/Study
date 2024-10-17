af620a67d324e55d0c59a22640784b2670d2d36f
박주원 <qkrwndnjs7308@gmail.com>
2024-10-17 11:15:18 +0900

    공부

--- Spring/S3/main/src/main/java/com/example/main/global/common/swagger/SwaggerBlog.java
+++ Spring/S3/main/src/main/java/com/example/main/global/common/swagger/SwaggerBlog.java
@@ -1,26 +1,28 @@
-/*package com.example.main.global.common.swagger;
+package com.example.main.global.common.swagger;
 
+import com.example.main.domain.file.domain.dto.FileResponse;
+import com.example.main.domain.file.domain.type.ImageType;
+import com.example.main.global.error.ErrorResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
-
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.ResponseEntity;
 
 @Tag(name = "intern", description = "인턴 모집 조회")
-public interface SwaggerIntern {
+public interface SwaggerBlog {
 
-    @ApiResponse({
+    @ApiResponses({
             @ApiResponse(
                     responseCode = "200",
                     description = "OK, 성공!",
                     content = @Content(
                             mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(
-                                    implementation = //보내는 response
-                            )
-
+                            schema = @Schema(implementation = FileResponse.class)
                     )
             ),
             @ApiResponse(
@@ -28,18 +30,13 @@ public interface SwaggerIntern {
                     description = "인턴 지원 목록을 조회할 수 없습니다.",
                     content = @Content(
                             mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(
-                                    implementation = // ErrorResponse.class
-                            )
+                            schema = @Schema(implementation = ErrorResponse.class)
                     )
             )
-
     })
     @Operation(
             summary = "인턴을 지원한 학생 목록 조회 API",
             description = "학생의 지원 받은 시간 순으로 지원 목록 검색합니다"
     )
-    // dto ( 쓰고 ) ex) internResponse getInterResponse(String internId)
-
+    ResponseEntity<FileResponse> saveImage(MultipartFile multipartFile, ImageType imageType);
 }
-*/
