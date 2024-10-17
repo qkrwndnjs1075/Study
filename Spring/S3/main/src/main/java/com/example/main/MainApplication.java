6209522870a9576dba593f4b30d2f9729e7fc925
박주원 <qkrwndnjs7308@gmail.com>
2024-10-17 11:15:52 +0900

    공부

--- Spring/S3/main/src/main/java/com/example/main/MainApplication.java
+++ Spring/S3/main/src/main/java/com/example/main/MainApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableFeignClients
 public class MainApplication {
 
     public static void main(String[] args) {
