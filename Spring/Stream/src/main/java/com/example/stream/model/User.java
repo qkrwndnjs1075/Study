7b3f05c90cb6eee15e4b6f18f9958cb82b78469d
박주원 <qkrwndnjs7308@gmail.com>
2024-10-28 16:07:35 +0900

    nice

---
+++ Spring/Stream/src/main/java/com/example/stream/model/User.java
@@ -0,0 +1,22 @@
+package com.example.stream.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@Accessors
+public class User {
+
+    private int id;
+    private String name;
+    private String emailAddress;
+    private boolean isVerified;
+    private List<Integer> friendUserIds;
+
+}
