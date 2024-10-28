7288756e92b042e9b00aafc4d469009a0ed83a24
박주원 <qkrwndnjs7308@gmail.com>
2024-10-28 16:10:12 +0900

    a

--- Spring/Stream/src/main/java/com/example/stream/model/Order.java
+++ Spring/Stream/src/main/java/com/example/stream/model/Order.java
@@ -5,11 +5,27 @@ import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.Accessors;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 @Setter
 @Getter
 @ToString
 @Accessors(chain = true)
 public class Order {
 
-    
+    private Long id;
+    private LocalDateTime createAt;
+    private long createByUserId;
+    private OrderStatus status;
+    private BigDecimal amount;
+    private List<OrderLine> orderLines;
+
+
+    public enum OrderStatus {
+        CREATED,
+        IN_PROGRESS,
+        ERROR,
+        PROCESSED
+    }
 }
