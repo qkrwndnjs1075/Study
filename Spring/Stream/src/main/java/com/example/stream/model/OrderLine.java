2b5efdd9881126f270bec7e1652b9be8b323ba49
박주원 <qkrwndnjs7308@gmail.com>
2024-10-28 16:12:01 +0900

    good

--- Spring/Stream/src/main/java/com/example/stream/model/OrderLine.java
+++ Spring/Stream/src/main/java/com/example/stream/model/OrderLine.java
@@ -1,4 +1,26 @@
 package com.example.stream.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+@ToString
 public class OrderLine {
+
+    private long id;
+    private OrderLineType type;
+    private long productId;
+    private int quantity;
+    private BigDecimal amount;
+
+    public enum OrderLineType {
+        PURCHASE,
+        DISCOUNT
+    }
 }
