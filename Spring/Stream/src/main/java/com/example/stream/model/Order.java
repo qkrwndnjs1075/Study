3229df32d328b866a58d6eb120dc3f6522c970ff
박주원 <qkrwndnjs7308@gmail.com>
2024-10-28 16:08:49 +0900

    d

---
+++ Spring/Stream/src/main/java/com/example/stream/model/Order.java
@@ -0,0 +1,13 @@
+package com.example.stream.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+@Setter
+@Getter
+@ToString
+@Accessors(chain = true)
+public class Order {
+}
