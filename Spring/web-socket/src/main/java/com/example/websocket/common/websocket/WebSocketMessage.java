23321e85a01386ddf7e0d70dc77e758e07c84400
박주원 <qkrwndnjs7308@gmail.com>
2024-11-02 22:48:17 +0900

    소캣

---
+++ Spring/web-socket/src/main/java/com/example/websocket/common/websocket/WebSocketMessage.java
@@ -0,0 +1,25 @@
+package com.example.websocket.common.websocket;
+
+import com.example.websocket.chat.dto.ChatDto;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+
+@RequiredArgsConstructor
+public class WebSocketMessage {
+
+    private final WebSocketMessageType type;
+    private final ChatDto payload;
+
+    @JsonCreator
+    public webSocketMessage(
+            @JsonProperty("type") WebSocketMessageType type,
+            @JsonProperty("payload") ChatDto payload){
+        this.type = type;
+        this.payload = payload;
+    }
+}
