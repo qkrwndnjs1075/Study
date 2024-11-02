67e510e1ad8949546994361ca38ebabca9112bad
박주원 <qkrwndnjs7308@gmail.com>
2024-11-02 22:48:27 +0900

    소캣

---
+++ Spring/web-socket/src/main/java/com/example/websocket/common/config/RedisConfig.java
@@ -0,0 +1,39 @@
+package com.example.websocket.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+@Configuration
+public class RedisConfig {
+
+    /**
+     * 메시지를 수신하고 처리할 수 있게 해주는 컨테이너
+     * @param connectionFactory RedisConnectionFactory
+     * @return RedisMessageListenerContainer
+     */
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListener(
+            RedisConnectionFactory connectionFactory){
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+
+        return container;
+    }
+
+
+    /**
+     * Redis에 대한 기본적인 연결과 통신을 담당하는 클래스
+     * @param redisConnectionFactory RedisConnectionFactory
+     * @return StringRedisTemplate
+     */
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory){
+
+        return new StringRedisTemplate(redisConnectionFactory);
+    }
+}
