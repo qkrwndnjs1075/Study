91325494a16ef77208b168a8070e45218fc06846
박주원 <qkrwndnjs7308@gmail.com>
2024-11-03 19:24:40 +0900

    구현 됐냐?

--- Spring/web-socket/src/main/java/com/example/websocket/common/interceptor/WebSocketAuthInterceptor.java
+++ Spring/web-socket/src/main/java/com/example/websocket/common/interceptor/WebSocketAuthInterceptor.java
@@ -18,29 +18,35 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class WebSocketAuthInterceptor implements HandshakeInterceptor {
 
+    /**
+     * 웹소켓 연결 전 인터셉터
+     * Authorization 헤더를 확인하여 유저를 인증한다.
+     * 유저 명이 채팅유저로 시작하지 않으면 401을 반환한다.
+     * 유저 명이 채팅유저로 시작해서 인증된 유저라면 session에 username을 저장한다.
+     */
+
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response,
-                                   WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception{
-        if(request instanceof ServerHttpRequest){
+                                   WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        if (request instanceof ServerHttpRequest) {
             HttpServletRequest servletRequest = ((ServletServerHttpRequest) request).getServletRequest();
             HttpServletResponse servletResponse = ((ServletServerHttpResponse) response).getServletResponse();
 
             String username = servletRequest.getHeader("Authorization");
-            if(username != null && username.startsWith("chatUser")){
+            if (username != null && username.startsWith("chatUser")) {
                 attributes.put("username", username);
-
                 return true;
-            }else {
+            } else {
                 servletResponse.setStatus(401);
                 return false;
             }
         }
         return false;
     }
+
     @Override
-    public void afterHandShake(ServerHttpRequest request, HttpServletResponse response,
-                               WebSocketHandler wsHandler, Exception exception){
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response,
+                               WebSocketHandler wsHandler, Exception exception) {
 
     }
-
 }
