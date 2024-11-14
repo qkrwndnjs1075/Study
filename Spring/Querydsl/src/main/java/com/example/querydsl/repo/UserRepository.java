c24d78e68fa11addde0055e9d2585274113cd236
박주원 <qkrwndnjs7308@gmail.com>
2024-11-14 23:28:27 +0900

    q

--- Spring/Querydsl/src/main/java/com/example/querydsl/repo/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.querydsl.repo;
-
-import com.example.querydsl.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-
-}
