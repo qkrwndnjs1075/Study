37b87efdfc1ec51b746c53a64c636a57fe96f5b1
박주원 <qkrwndnjs7308@gmail.com>
2024-11-14 23:28:37 +0900

    q

--- Spring/Querydsl/src/main/java/com/example/querydsl/domain/User.java
+++ Spring/Querydsl/src/main/java/com/example/querydsl/domain/User.java
@@ -1,23 +1,19 @@
 package com.example.querydsl.domain;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
-@Getter
-@Setter
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long userNo;
 
-    private String name;
-    private int age;
+    private String userId;
 
-
+    private String userName;
 }
