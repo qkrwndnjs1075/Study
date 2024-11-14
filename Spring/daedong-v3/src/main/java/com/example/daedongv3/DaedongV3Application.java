fe4556186b246e72b20500f8dd31a932f695ed12
박주원 <qkrwndnjs7308@gmail.com>
2024-11-14 21:22:18 +0900

    d

---
+++ Spring/daedong-v3/src/main/java/com/example/daedongv3/DaedongV3Application.java
@@ -0,0 +1,13 @@
+package com.example.daedongv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaedongV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaedongV3Application.class, args);
+    }
+
+}
