9adefd1d35208109099294df2b70819b6a3da8f3
박주원 <qkrwndnjs7308@gmail.com>
2024-11-14 23:29:43 +0900

    q

--- Spring/Querydsl/src/main/java/com/example/querydsl/dto/BoardDto.java
+++ Spring/Querydsl/src/main/java/com/example/querydsl/dto/BoardDto.java
@@ -1,4 +1,23 @@
 package com.example.querydsl.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class BoardDto {
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+
+    @QueryProjection
+    public BoardDto(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
 }
