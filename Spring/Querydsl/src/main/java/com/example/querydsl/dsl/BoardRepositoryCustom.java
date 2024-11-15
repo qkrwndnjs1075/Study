c92e048c5247e772f45676c6a6c18a0780fec143
박주원 <qkrwndnjs7308@gmail.com>
2024-11-15 22:28:40 +0900

    b

---
+++ Spring/Querydsl/src/main/java/com/example/querydsl/dsl/BoardRepositoryCustom.java
@@ -0,0 +1,13 @@
+package com.example.querydsl.dsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class BoardRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+}
