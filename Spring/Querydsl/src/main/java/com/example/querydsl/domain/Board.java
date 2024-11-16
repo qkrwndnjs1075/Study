11c09f8cb5d85b11b4d81be81ce82f460ee8af13
박주원 <qkrwndnjs7308@gmail.com>
2024-11-16 19:33:57 +0900

    d
    d

--- Spring/Querydsl/src/main/java/com/example/querydsl/domain/Board.java
+++ Spring/Querydsl/src/main/java/com/example/querydsl/domain/Board.java
@@ -1,23 +1,23 @@
-package com.example.querydsl.domain;
-
-import jakarta.persistence.*;
-import lombok.*;
-
-@Entity
-@Getter @Setter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
-public class Board {
-
-    @Id
-    private Long id;
-
-    private String title;
-
-    private String content;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_no")
-    private User user;
-
-}
+//package com.example.querydsl.domain;
+//
+//import jakarta.persistence.*;
+//import lombok.*;
+//
+//@Entity
+//@Getter @Setter
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@AllArgsConstructor(access = AccessLevel.PROTECTED)
+//public class Board {
+//
+//    @Id
+//    private Long id;
+//
+//    private String title;
+//
+//    private String content;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_no")
+//    private User user;
+//
+//}
